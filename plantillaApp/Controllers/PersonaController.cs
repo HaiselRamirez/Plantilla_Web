8c1d49e6947a64eb6a3b4ce80603142e6a222a53
Haisel  Ramirez <haiselramirez@outlook.com>
2021-06-08 01:39:27 -0400

    Inicio de la aplicacion

---
+++ plantillaApp/Controllers/PersonaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace plantillaApp.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+		    {
+          return View();
+		    }
+
+		    #region Metodo para guardar los datos de una persona
+		    [HttpPost]
+        public ActionResult Guardar(string tCedula, string tNombre, string tApellidos, string tFetTelefono, string tEmail, string tFecha, string tSexo, string tDireccion, string tCiudad, string tNacionalidad)
+        {
+          ViewBag.mensaje = "Hola " + tNombre + " Se guardaron los datos";
+          return View("Crear");
+        }
+		    #endregion
+	}
+}
